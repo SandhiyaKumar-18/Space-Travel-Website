9ddd1fbe67b1e1a85ebcc230dd7e6a665db9eeae
SandhiyaKumar-18 <96000268+SandhiyaKumar-18@users.noreply.github.com>
2022-03-11 20:14:30 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// when someone clicks the hamburger menu
+// if the nav is closed, open it
+// if the nav is open, close it
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+    
+    console.log(navToggle.getAttribute('aria-expanded'));
+})
---
+++ src/tabs.js
@@ -0,0 +1,13 @@
+const tabLists = document.querySelector('[role="tablist"]')
+const tabs = tabLists.querySelectorAll('[role="tab"]')
+
+tabLists.addEventListener('keydown',(e) =>{
+    const keydownLeft =37;
+    const keydownRight = 39;
+    // change the tabindex of the current tab to -1
+    
+    
+    // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownLeft || e.keyCode ===keyd)
+    // if the left key is pushed, move to the next tab on the left
+})
